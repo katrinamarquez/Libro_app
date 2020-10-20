48d16b1da1fb68234692d9e1a8b6a9ce53c08f96
katrinamarquez <katrina.l.marquez@gmail.com>
2020-10-20 23:41:12 +1000

    upload wrong files

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 const mongoose = require("mongoose")
 const bookRouter = require("./routes/book_routes")
-const api_helper = require('./API_helper')
+// const api_helper = require('./API_helper')
 const Book = require("./models/book.js")
 // import {v4 as uuidv4} from 'uuid'
+require('./models/Users');
+require('./config/passport');
+app.use(require('./routes'));
 
 const port = 3000
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({ secret: 'passport-tutorial', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 const dbConn = process.env.MONGODB_URI || "mongodb://localhost/libro_app"
 // when we need to deploy:
@@ -51,18 +55,6 @@ app.get("/register", (req,res)=> {
 	res.render("register")
 })
 
-app.post("/books/new", (req, res) => {
-  var addedBook = {
-		title: req.body.title,
-		author: req.body.author,
-		category: req.body.category,
-		published_year: req.body.published_year
-	}
-	new Book(addedBook).save()
-	console.log(addedBook)
-	res.redirect("/")
-})
-
 // app.get("/books", (req, res)=> {
 // 	res.render("books")
 // })
@@ -75,8 +67,6 @@ app.get("/books/addBook", (req, res)=>{
 	res.render("addBook")
 })
 
-
-
 // app.get('/getAPIResponse', (req, res) => {
 // 	api_helper.make_API_call('https://jsonplaceholder.typicode.com/todos/1')
 // 	.then(response => {
@@ -88,7 +78,6 @@ app.get("/books/addBook", (req, res)=>{
 // })
 
 app.use("/books", bookRouter)
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
